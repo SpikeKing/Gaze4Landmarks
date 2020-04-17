b631f12dd931220d958f246d4dc8324374c5e431
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-04-17 14:38:28 +0800

    format

--- camera.py
+++ camera.py
@@ -12,6 +12,7 @@ from mtcnn.detector import detect_faces, show_bboxes
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def main(args):
     checkpoint = torch.load(args.model_path, map_location=device)
     plfd_backbone = PFLDInference().to(device)
@@ -30,16 +31,16 @@ def main(args):
         bounding_boxes, landmarks = detect_faces(img)
         for box in bounding_boxes:
             score = box[4]
-            x1, y1, x2, y2 = (box[:4]+0.5).astype(np.int32)
+            x1, y1, x2, y2 = (box[:4] + 0.5).astype(np.int32)
             w = x2 - x1 + 1
             h = y2 - y1 + 1
 
-            size = int(max([w, h])*1.1)
-            cx = x1 + w//2
-            cy = y1 + h//2
-            x1 = cx - size//2
+            size = int(max([w, h]) * 1.1)
+            cx = x1 + w // 2
+            cy = y1 + h // 2
+            x1 = cx - size // 2
             x2 = x1 + size
-            y1 = cy - size//2
+            y1 = cy - size // 2
             y2 = y1 + size
 
             dx = max(0, -x1)
@@ -55,7 +56,7 @@ def main(args):
             cropped = img[y1:y2, x1:x2]
             if (dx > 0 or dy > 0 or edx > 0 or edy > 0):
                 cropped = cv2.copyMakeBorder(cropped, dy, edy, dx, edx, cv2.BORDER_CONSTANT, 0)
-            
+
             cropped = cv2.resize(cropped, (112, 112))
 
             input = cv2.resize(cropped, (112, 112))
@@ -70,7 +71,6 @@ def main(args):
         cv2.imshow('0', img)
         if cv2.waitKey(10) == 27:
             break
-
 
 
 def parse_args():
