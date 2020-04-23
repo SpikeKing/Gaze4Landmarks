3afd22404d94820a9829caccfb62e0725fb8394f
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-04-23 15:25:58 +0800

    修改测试环境

--- vid_predicter.py
+++ vid_predicter.py
@@ -11,7 +11,7 @@ import cv2
 
 from gaze_predicter import GazePredicter
 from root_dir import VIDS_DIR
-from utils.project_utils import mkdir_if_not_exist, get_current_time_str
+from utils.project_utils import mkdir_if_not_exist, get_current_time_str, traverse_dir_files
 from utils.video_utils import init_vid, write_video
 
 
@@ -25,6 +25,7 @@ class VidPredicter(object):
     def predict_path(self, path):
         print('[Info] 视频路径: {}'.format(path))
         cap, n_frame, fps, h, w = init_vid(path)
+        print('[Info] fps: {}, h: {}, w: {}'.format(fps, h, w))
         # n_frame = 100
         # img_list = []
         for i in range(0, n_frame):
@@ -44,11 +45,26 @@ class VidPredicter(object):
             cv2.imwrite(img_path, img_draw)
             print('[Info] 帧预测完成: {} / {}'.format(str(i), time.time() - s_time))
 
+    def write_frames_to_vid(self, frames_folder, out_vid_path):
+        paths_list, names_list = traverse_dir_files(frames_folder)
+
+        img_list = []
+        for path, name in zip(paths_list, names_list):
+            img = cv2.imread(path)
+            img_list.append(img)
+
+        # fps: 29, h: 1280, w: 720
+        write_video(out_vid_path, img_list, 29, 1280, 720)
+
 
 def main():
-    vid_path = os.path.join(VIDS_DIR, 'vid_no_glasses.mp4')
+    vid_path = os.path.join(VIDS_DIR, 'vid_glasses.mp4')
     vp = VidPredicter()
     vp.predict_path(vid_path)
+
+    # out_vid_path = os.path.join(VIDS_DIR, 'vid_no_glasses.out.mp4')
+    # frames_folder = os.path.join(VIDS_DIR, 'frames-20200423')
+    # vp.write_frames_to_vid(frames_folder, out_vid_path)
 
 
 if __name__ == '__main__':
